19e929d74339d4082729e3b749cc24c7843b9af4
cristian <cristian.tenorio@uisek.edu.ec>
2026-07-16 18:53:14 -0500

    Configurar rutas base con React Router

---
+++ src/pages/ArtistForm.jsx
@@ -0,0 +1,5 @@
+function ArtistForm() {
+  return <h1>Formulario de Artista</h1>
+}
+
+export default ArtistForm
---
+++ src/pages/ArtistList.jsx
@@ -0,0 +1,5 @@
+function ArtistList() {
+  return <h1>Listado de Artistas</h1>
+}
+
+export default ArtistList
